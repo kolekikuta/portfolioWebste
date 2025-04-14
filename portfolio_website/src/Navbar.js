ffffe7dd4beaa50745608e8c2f94a23f9f79f695
kolekikuta <kolekikuta@gmail.com>
2025-04-14 15:23:56 -0700

    contact link added to navbar

--- portfolio_website/src/Navbar.js
+++ portfolio_website/src/Navbar.js
@@ -17,12 +17,21 @@ export default function Navbar() {
         link.click();
     };
 
+    const handleContact = () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    };
+
+
     return (
         <header className="w-full text-center py-3 fixed top-0 left-0 z-50 bg-navbar-gradient flex justify-between items-center content-center px-6">
         <h2 className="text-xl font-medium cursor-pointer my-0" onClick={() => handleNavigation('/')}>Kole Kikuta</h2>
         <ul className="flex justify-center space-x-6 text-lg font-medium justify-center content-center align-middle">
             <li onClick={() => handleNavigation('/about')} className="cursor-pointer underline-middle-expand font-normal">About</li>
             <li onClick={() => handleNavigation('/projects')} className="cursor-pointer underline-middle-expand font-normal">Projects</li>
+            <li onClick={() => handleContact()} className="cursor-pointer underline-middle-expand font-normal">Contact</li>
             <li onClick={() => handleResumeOpen()} className="cursor-pointer underline-middle-expand font-normal">Resume</li>
         </ul>
       </header>
